86b0f1ec9caeb2ac7e5fa232edf46324ccecdc06
Sanny Sanoff <san@sysdate.com>
2025-07-30 19:25:58 +0200

    work ok

--- example.py
+++ example.py
@@ -1,8 +1,10 @@
+import sys
 from skimage.io import imread, imsave
 from smude import Smude
 
 smude = Smude(use_gpu=False, binarize_output=True)
 
-image = imread('images/input_fullsize.jpg')
+input_file = sys.argv[1] if len(sys.argv) > 1 else 'images/input_fullsize.jpg'
+image = imread(input_file)
 result = smude.process(image)
 imsave('result.png', result)
